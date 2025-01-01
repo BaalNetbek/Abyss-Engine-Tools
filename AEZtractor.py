cc027741a7e4c121f87731f63bc81817aa903c13
Baal <zzztomek@o2.pl>
2025-01-02 00:52:03 +0100

    AEZtractor unpacks compressed files

--- AEZtractor.py
+++ AEZtractor.py
@@ -1,31 +1,39 @@
 '''
 AEZtractor - functions for unpacking and repacking .aez files. 
-I know of only one which is res.aez packing assets of GoF3D symbian version.
+res.aez files can be found GoF3D Symbian and Zeebo versions. GoF2 for Meego and mabey some other Abyss Engine games.
 
 You can use it directly running the this .py file after uncommenting bottom instructions.
 '''
 
 import os
 import struct
+import zlib
 
 def unpack_aez(input_file = 'res.aez', verbose = False):
     with open(input_file, 'rb') as f:
         while True:
             if verbose: print("\ncurnt adr: ",hex(f.tell()))
-            path_size_byte = f.read(1)
-            path_size = int.from_bytes(path_size_byte, byteorder='little', signed=False)
+            try:
+                path_size = struct.unpack('B', f.read(1))[0]
+            except struct.error as e:
+                break
             if verbose: print("path size: ", path_size)
             file_path = f.read(path_size).decode('utf-8')
             if verbose: print("path size: ", file_path) 
-            file_size_bytes = f.read(4)
-            file_size = int.from_bytes(file_size_bytes, byteorder='little', signed=False)
-            if verbose: print("file size: ", file_size)
-            const_bytes = f.read(4)
-            
-            if(path_size == 0 and file_size == 0):
-                break
 
-            file_data = f.read(file_size)
+            uncompressed_size = struct.unpack('<I', f.read(4))[0]
+            if verbose: print("file size: ", uncompressed_size)
+            compressed_size = struct.unpack('<i', f.read(4))[0]
+            
+            if compressed_size != -1:
+                if verbose: 
+                    print("comp size: ", compressed_size)
+                    print("comp ratio: ", round(compressed_size/uncompressed_size, 2))
+                unk = f.read(10)
+                compressed_data = f.read(compressed_size-10)
+                file_data = zlib.decompress(compressed_data, -zlib.MAX_WBITS)
+            else:
+                file_data = f.read(uncompressed_size)
             file_path = os.getcwd()+file_path
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
             if verbose: print(file_path)
@@ -41,21 +49,14 @@ def pack_aez(input_dir = os.getcwd()+"/data", output_file = "new_res.aez", verbo
                 rel_path = os.path.relpath(file_path, input_dir)
                 rel_path = '/' + os.path.basename(input_dir) + '/' + rel_path.replace('\\', '/')
 
-                # Write path size
-                path_size = len(rel_path)
-                f.write(struct.pack('B', path_size))
-
-                # Write path
+                f.write(struct.pack('B', len(rel_path)))
                 f.write(rel_path.encode('utf-8'))
-
-                # Write file size
                 file_size = os.path.getsize(file_path)
                 f.write(struct.pack('<I', file_size))
 
-                # Write constant bytes (4 bytes)
-                f.write(b'\xFF\xFF\xFF\xFF')
+                # Mark uncompressed files with 0xffffffff
+                f.write(struct.pack('<i', -1))
 
-                # Write file data
                 with open(file_path, 'rb') as in_f:
                     f.write(in_f.read())
 
@@ -64,17 +65,14 @@ def pack_aez(input_dir = os.getcwd()+"/data", output_file = "new_res.aez", verbo
 
         # Write end-of-archive marker (path size and file size both 0)
         f.write(struct.pack('BB', 0, 0))
-
     return None
 
 # Example uses:
-
+"""unpack res.aez if it is in the same directory as the script"""
 #unpack_aez('res.aez') 
-#will unpack res.aez if it is in the same directory as the script
 
-
-#will pack the folder data in script's current directory to a new archive
+"""pack the folder data in script's current directory to a new archive"""
 #pack_aez('data', "new_res.aez") 
 
-#excacly the same calling the function without arguments
+"""excacly the same calling the function without arguments"""
 #pack_aez()
